60b7db598d57c94eef7a88ec9769b47cd868526d
byObin <byObin26@gmail.com>
2022-01-15 23:50:22 +0900

    string1.c

---
+++ string1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	char str1[6] = "Seoul";
+	char str2[3] = { 'i','s's'\0' };
+	char str3[] = "the capital city of Korea.";
+
+	printf("%s %s %s\n", str1, str2, str3);
+}
